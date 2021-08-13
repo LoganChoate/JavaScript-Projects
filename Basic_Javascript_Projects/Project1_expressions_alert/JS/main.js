54087e64e42c6b255c70d05d97cdf6a5e9d2d07d
LoganChoate <loganvchoate@gmail.com>
2021-08-12 21:58:13 -0600

    Update main.js
    Added several other variable definitions and document.write calls.

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,15 +1,29 @@
-window.alert("Hello World!")
+window.alert("Hello World!");
 
-document.write("Hello World-2!")
+document.write("Hello World-2!");
 
-var A = "This is a string."
+var A = "This is a string.";
 
 document.write(A);
 
-var B = "This is only my second string ever!"
+var B = "This is only my second string ever!";
 
 document.write(B);
 
-var C = "This can\'t be the way it ends!"
+var C = "This can\'t be the way it ends!";
 
 document.write(C);
+
+var D = "This sentence is actually; " + "made up of two strings put together!";
+
+document.write(D);
+
+var E = "Elephant", F = "Frog", G = "Giraffe";
+var E = E.fontcolor("grey");
+var F = F.fontcolor("green");
+var G = G.fontcolor("gold");
+
+document.write(F);
+
+document.write(3+3);
+
