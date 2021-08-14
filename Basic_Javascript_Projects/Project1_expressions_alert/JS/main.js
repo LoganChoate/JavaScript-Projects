146ada739ad6bedfc34fb060da6556d53843039a
LoganChoate <loganvchoate@gmail.com>
2021-08-14 15:37:06 -0600

    Update main.js
    Corrected newline statements.

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -2,46 +2,58 @@ window.alert("Hello World!"); // This is an alert message that says Hello World!
 document.write("\n");
 
 document.write("Hello World-2!"); // this is a document write message that says Hello World
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 var A = "This is a string."; //This is a variable declaration
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 document.write(A); // This is a document write 
                     //message that displays the text of the previous variable.
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 var B = "This is only my second string ever!"; //This is another variable definition
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 document.write(B); //This document write message displays the previous definition
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 var C = "This can\'t be the way it ends!"; //This is a variable definition 
                                             //with special chars
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 document.write(C); // this is a document write
                     // message that displays the previous definition
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 var D = "This sentence is actually; " + "made up of two strings put together!"; //This is
 //is a concactenation of two defined variable strings
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 document.write(D); // this is a document message to display the concactenated strings
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 var E = "Elephant", F = "Frog", G = "Giraffe"; //These variables have 
                                                 //colors attached to them
 var E = E.fontcolor("grey");
 var F = F.fontcolor("green");
 var G = G.fontcolor("gold");
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 document.write(F); //This is a document write message that displays the colored variables
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
 
 document.write(3+3); //This is a statement that displays
                     // the output of an arithmetic expression. 
-document.write("\n");
+document.write("<br>");
+document.write("<br>");
