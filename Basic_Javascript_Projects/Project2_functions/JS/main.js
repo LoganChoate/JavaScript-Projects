dff4fbd1bc5a65603aeb15f90ccd7c7a89aa68a0
LoganChoate <loganvchoate@gmail.com>
2021-08-13 20:45:47 -0600

    Update main.js
    Added another Function to main.js

--- Basic_Javascript_Projects/Project2_functions/JS/main.js
+++ Basic_Javascript_Projects/Project2_functions/JS/main.js
@@ -1,5 +1,13 @@
-function My_First_Function(){
+function My_First_Function()
+{
     var str = "This text is green!";
     var result = str.fontcolor("green");
     document.getElementById("Green_Text").innerHTML = result;
+}
+
+function myFunction()
+{
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
