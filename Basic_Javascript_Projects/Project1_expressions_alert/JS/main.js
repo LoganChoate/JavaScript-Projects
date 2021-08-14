98c0e9d67f4058704dbfde0a3c3baa6e3f322fc0
LoganChoate <loganvchoate@gmail.com>
2021-08-13 20:25:52 -0600

    Update main.js
    Numerous updates to the main.js file to fulfill assignment parameters.

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,29 +1,35 @@
-window.alert("Hello World!");
+window.alert("Hello World!"); // This is an alert message that says Hello World!
 
-document.write("Hello World-2!");
+document.write("Hello World-2!"); // this is a document write message that says Hello World
 
-var A = "This is a string.";
+var A = "This is a string."; //This is a variable declaration
 
-document.write(A);
+document.write(A); // This is a document write 
+                    //message that displays the text of the previous variable.
 
-var B = "This is only my second string ever!";
+var B = "This is only my second string ever!"; //This is another variable definition
 
-document.write(B);
+document.write(B); //This document write message displays the previous definition
 
-var C = "This can\'t be the way it ends!";
+var C = "This can\'t be the way it ends!"; //This is a variable definition 
+                                            //with special chars
 
-document.write(C);
+document.write(C); // this is a document write
+                    // message that displays the previous definition
 
-var D = "This sentence is actually; " + "made up of two strings put together!";
+var D = "This sentence is actually; " + "made up of two strings put together!"; //This is
+//is a concactenation of two defined variable strings
 
-document.write(D);
+document.write(D); // this is a document message to display the concactenated strings
 
-var E = "Elephant", F = "Frog", G = "Giraffe";
+var E = "Elephant", F = "Frog", G = "Giraffe"; //These variables have 
+                                                //colors attached to them
 var E = E.fontcolor("grey");
 var F = F.fontcolor("green");
 var G = G.fontcolor("gold");
 
-document.write(F);
+document.write(F); //This is a document write message that displays the colored variables
 
-document.write(3+3);
+document.write(3+3); //This is a statement that displays
+                    // the output of the math written here 
 
