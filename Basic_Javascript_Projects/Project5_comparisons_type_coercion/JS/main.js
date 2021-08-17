6274bb0b09b92424432073ce6d6c03ed61cfad5b
LoganChoate <loganvchoate@gmail.com>
2021-08-16 20:32:05 -0600

    Create main.js
    Created a file named "main.js" that demonstrates understanding of comparisons and type coercion.

---
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,15 @@
+/**This code uses a document.write call to 
+ * display a string variable.
+ */
+document.write(typeof "Word");
+document.write("<br>");
+
+/**This code uses a document.write call to
+ * display a numerical variable.
+ */
+document.write(typeof 3);
+document.write("<br>");
+
+/**This code demonstrates type coercion */
+document.write("10" + 5);
+document.write("<br>");
