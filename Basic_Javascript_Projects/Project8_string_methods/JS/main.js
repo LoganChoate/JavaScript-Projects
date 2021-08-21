7f6ab7bda8558fe5ccbfeff4d752af71d8592961
LoganChoate <loganvchoate@gmail.com>
2021-08-20 22:04:46 -0600

    update
    Added the concatenate method.

--- Basic_Javascript_Projects/Project8_string_methods/JS/main.js
+++ Basic_Javascript_Projects/Project8_string_methods/JS/main.js
@@ -1,4 +1,16 @@
 /**
+ * this code shows the concatenate method.
+ */
+function full_Sentence(){
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+/**
  * This code shows the slice method and how it can be used to 
  * chop up strings. 
 */
