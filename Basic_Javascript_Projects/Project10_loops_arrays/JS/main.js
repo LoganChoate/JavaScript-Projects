6e2216fdfcba23530e95cae851d66b4b58b9bf9c
LoganChoate <loganvchoate@gmail.com>
2021-08-21 15:24:04 -0600

    Update main.js
    Added a function that will concatenates a string with the value contained in a specific index of an array.

--- Basic_Javascript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_Javascript_Projects/Project10_loops_arrays/JS/main.js
@@ -29,3 +29,16 @@ function for_Loop()
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+
+/**
+ * This code will display a specific element of an array.
+ */
+function cat_pics()
+{
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture the cat is " + Cat_Picture[2] + ".";
+}
