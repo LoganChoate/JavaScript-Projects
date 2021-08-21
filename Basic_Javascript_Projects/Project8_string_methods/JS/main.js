bb418e7983a107ccc65684f57f5f6dd07b0435f7
LoganChoate <loganvchoate@gmail.com>
2021-08-20 21:31:39 -0600

    Create main.js
    Creates a main.js to show usage of string methods.

---
+++ Basic_Javascript_Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,8 @@
+/**
+ * This code take 
+*/
+function slice_Method(){
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
