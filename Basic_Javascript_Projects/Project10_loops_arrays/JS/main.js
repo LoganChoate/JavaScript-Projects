698d67c2f73dbfd80437a8ac396c8098f418fe45
LoganChoate <loganvchoate@gmail.com>
2021-08-21 16:10:43 -0600

    Update main.js
    Added a let function, a break and continue statement, respectively.

--- Basic_Javascript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_Javascript_Projects/Project10_loops_arrays/JS/main.js
@@ -56,3 +56,52 @@ function constant_function()
      + Musical_Instrument.type + " was "
       + Musical_Instrument.price + ".";
 }
+
+/**This code creates an object, defines is,
+ * and then uses a function to diplay its contents.
+ */
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + 
+        this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
+/**
+ * This code uses a break statement to exit a loop.
+ */
+function break_Loop()
+{
+    var text = "";
+    for(let i = 0; i < 10; i++)
+    {
+            if(i === 3)
+            {
+                break;
+            }
+            text += "The number is " + i + "<br>";
+            document.getElementById("Break").innerHTML = text;
+    }
+}
+
+/**
+ * This code uses a continue statement to skip a value.
+ */
+function continue_Loop()
+{
+    var text = "";
+    for(let i = 0; i < 10; i++)
+    {
+            if(i === 3)
+            {
+                continue;
+            }
+            text += "The number is " + i + "<br>";
+            document.getElementById("Continue").innerHTML = text;
+    }
+}
